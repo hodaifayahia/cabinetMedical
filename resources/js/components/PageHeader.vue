1587b6245c0da383ce15e28d3c1d46f531ed31d2
hodaifayahia <91214366+hodaifayahia@users.noreply.github.com>
2026-08-08 22:58:27 +0100

    UI/UX overhaul: resources/js/components/PageHeader.vue

---
+++ resources/js/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Heading from '@/components/Heading.vue';
+
+type Props = {
+    title: string;
+    description?: string;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+    <header
+        class="flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between"
+    >
+        <Heading :title="title" :description="description" />
+        <div v-if="$slots.actions" class="flex flex-wrap items-center gap-2">
+            <slot name="actions" />
+        </div>
+    </header>
+</template>
